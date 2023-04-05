7edb776f15d531002322dd14f3d385c95fc92eb5
padamkirankumar <68222338+padamkirankumar@users.noreply.github.com>
2023-04-05 17:30:39 +0530

    Update db.php

--- bookstore/db.php
+++ bookstore/db.php
@@ -1,13 +1,13 @@
 <?php
-$servername = "localhost";
-$username = "apptieib_books_kiran";
-$password = "books_kiran";
+$servername = "db";
+$username = "root";
+$password = "test";
 
 $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "USE apptieib_books_kiran";
+$sql = "myDb";
 $conn->query($sql);
 ?>
